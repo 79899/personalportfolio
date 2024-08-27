7d642cfd0ab11634a3599caaa2aa26bf3ad05baa
Saisindhu <saisindhu466@gmail.com>
2024-08-27 16:45:25 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Frontend Developer","Python Developer","Bussiness Women"],
+    tyepeSpeed:100,
+    backSpeed:100,
+    backDelay:100,
+    loop:true
+});
